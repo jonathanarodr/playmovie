46eba329b788893520ea0d71a88217054c06f82f
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2019-11-05 23:58:20 -0300

    Padroniza uso da classe R

--- app/src/main/java/br/com/jonathanarodr/playmovie/ui/MainActivity.kt
+++ app/src/main/java/br/com/jonathanarodr/playmovie/ui/MainActivity.kt
@@ -10,10 +10,10 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.jonathanarodr.playmovie.R.id
+import br.com.jonathanarodr.playmovie.R
 import br.com.jonathanarodr.playmovie.R.layout
 import br.com.jonathanarodr.playmovie.model.Movie
-import br.com.jonathanarodr.playmovie.ui.DetailActivity.STATE_FAVORITE
+import br.com.jonathanarodr.playmovie.ui.DetailActivity.Companion.STATE_FAVORITE
 import br.com.jonathanarodr.playmovie.util.NetworkUtils.hasNetworkConnection
 import br.com.jonathanarodr.playmovie.util.findViewByLazy
 import br.com.jonathanarodr.playmovie.viewmodel.MovieViewModel
@@ -22,19 +22,19 @@ import javax.inject.Inject
 
 class MainActivity : BaseActivity(), MovieAdapterOnClickHandler {
 
+    @Inject
+    private lateinit var movieViewModel: MovieViewModel
+    private val movieAdapter: MovieAdapter by lazy { MovieAdapter(this) }
+    private val loadingIndicator: ProgressBar by findViewByLazy(R.id.pb_loading_indicator)
+    private val messageError: LinearLayout by findViewByLazy(R.id.ll_message_error)
+    private val listMovie: RecyclerView by findViewByLazy(R.id.rv_list_movie)
+    private val tryAgain: Button by findViewByLazy(R.id.bt_try_again)
+    private val navigation: BottomNavigationView by findViewByLazy(R.id.bottom_navigation)
+
     companion object {
         private const val STATE_BUNDLE_KEY = "state_bundle"
         private const val STATE_MOVIE_KEY = "state_movies"
     }
-
-    @Inject
-    private lateinit var movieViewModel: MovieViewModel
-    private val movieAdapter: MovieAdapter by lazy { MovieAdapter(this) }
-    private val loadingIndicator: ProgressBar by findViewByLazy(id.pb_loading_indicator)
-    private val messageError: LinearLayout by findViewByLazy(id.ll_message_error)
-    private val listMovie: RecyclerView by findViewByLazy(id.rv_list_movie)
-    private val tryAgain: Button by findViewByLazy(id.bt_try_again)
-    private val navigation: BottomNavigationView by findViewByLazy(id.bottom_navigation)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,7 +66,7 @@ class MainActivity : BaseActivity(), MovieAdapterOnClickHandler {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(Intent.EXTRA_INTENT, movie)
 
-        if (navigation.selectedItemId == id.action_favorite_movie) {
+        if (navigation.selectedItemId == R.id.action_favorite_movie) {
             intent.putExtra(STATE_FAVORITE, movie.id)
         }
 
@@ -76,23 +76,23 @@ class MainActivity : BaseActivity(), MovieAdapterOnClickHandler {
     private fun bindListeners() {
         tryAgain.setOnClickListener {
             when (navigation.selectedItemId) {
-                id.action_popular_movie -> searchPopularMovie()
-                id.action_top_rate_movie -> searchTopRatedMovie()
-                id.action_favorite_movie -> searchFavoriteMovie()
+                R.id.action_popular_movie -> searchPopularMovie()
+                R.id.action_top_rate_movie -> searchTopRatedMovie()
+                R.id.action_favorite_movie -> searchFavoriteMovie()
             }
         }
 
         navigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                id.action_popular_movie -> {
+                R.id.action_popular_movie -> {
                     searchPopularMovie()
                     true
                 }
-                id.action_top_rate_movie -> {
+                R.id.action_top_rate_movie -> {
                     searchTopRatedMovie()
                     true
                 }
-                id.action_favorite_movie -> {
+                R.id.action_favorite_movie -> {
                     searchFavoriteMovie()
                     true
                 }
@@ -115,7 +115,7 @@ class MainActivity : BaseActivity(), MovieAdapterOnClickHandler {
     }
 
     private fun loadMovies(savedInstanceState: Bundle?) {
-        if (navigation.selectedItemId != id.action_favorite_movie
+        if (navigation.selectedItemId != R.id.action_favorite_movie
             && savedInstanceState == null
             || !savedInstanceState!!.containsKey(STATE_BUNDLE_KEY)
         ) {
