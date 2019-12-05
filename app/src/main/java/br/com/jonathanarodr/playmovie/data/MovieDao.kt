854a96f740abbab64f9cffb32f8b788cb547aad9
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2019-12-04 22:37:46 -0300

    Corrige problema de conflito caso id do filme esteja presente nas duas abas do app

--- app/src/main/java/br/com/jonathanarodr/playmovie/data/MovieDao.kt
+++ app/src/main/java/br/com/jonathanarodr/playmovie/data/MovieDao.kt
@@ -1,10 +1,7 @@
 package br.com.jonathanarodr.playmovie.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import br.com.jonathanarodr.playmovie.model.Movie
 
 @Dao
@@ -12,7 +9,7 @@ interface MovieDao {
     @Query("select * from movies order by title")
     fun listMovies(): LiveData<List<Movie>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertMovie(movie: Movie)
 
     @Delete
