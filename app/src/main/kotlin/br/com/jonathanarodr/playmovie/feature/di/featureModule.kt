92cb45d72d68e53758cb79abb6a6ac9dd178f9ec
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-07-05 23:27:55 -0300

    Add injection of classes about repository data source

--- app/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/di/featureModule.kt
+++ app/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/di/featureModule.kt
@@ -1,12 +1,20 @@
 package br.com.jonathanarodr.playmovie.feature.di
 
+import br.com.jonathanarodr.playmovie.feature.repository.MovieRepositoryImpl
+import br.com.jonathanarodr.playmovie.feature.repository.local.MovieLocalDataSource
+import br.com.jonathanarodr.playmovie.feature.repository.local.database.AppDataBase
 import br.com.jonathanarodr.playmovie.feature.repository.remote.MovieRemoteDataSource
-import br.com.jonathanarodr.playmovie.feature.repository.remote.MovieService
+import br.com.jonathanarodr.playmovie.feature.repository.remote.api.MovieApi
 import br.com.jonathanarodr.playmovie.network.NetworkClientService
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val featureModule = module {
 
-    single { NetworkClientService.providerService(MovieService::class.java) }
+    single { AppDataBase(androidApplication()) }
+    single { get<AppDataBase>().movieDao() }
+    single { NetworkClientService.providerService(MovieApi::class.java) }
+    single { MovieLocalDataSource(get()) }
     single { MovieRemoteDataSource(get()) }
+    single { MovieRepositoryImpl(get(), get()) }
 }
