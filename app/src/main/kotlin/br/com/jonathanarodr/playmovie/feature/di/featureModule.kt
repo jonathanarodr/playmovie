ceeccefc98e4338cbb721544528fe320bbfbd646
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-07-08 08:47:09 -0300

    Mapping injections about feature module

--- app/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/di/featureModule.kt
+++ app/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/di/featureModule.kt
@@ -1,12 +1,18 @@
 package br.com.jonathanarodr.playmovie.feature.di
 
+import br.com.jonathanarodr.playmovie.feature.domain.usecase.DetailUseCase
+import br.com.jonathanarodr.playmovie.feature.domain.usecase.MovieUseCase
+import br.com.jonathanarodr.playmovie.feature.repository.MovieRepository
 import br.com.jonathanarodr.playmovie.feature.repository.MovieRepositoryImpl
 import br.com.jonathanarodr.playmovie.feature.repository.local.MovieLocalDataSource
 import br.com.jonathanarodr.playmovie.feature.repository.local.db.AppDataBase
 import br.com.jonathanarodr.playmovie.feature.repository.remote.MovieRemoteDataSource
 import br.com.jonathanarodr.playmovie.feature.repository.remote.api.MovieApi
+import br.com.jonathanarodr.playmovie.feature.ui.viewmodel.DetailViewModel
+import br.com.jonathanarodr.playmovie.feature.ui.viewmodel.MovieViewModel
 import br.com.jonathanarodr.playmovie.network.NetworkClientService
 import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val featureModule = module {
@@ -16,5 +22,9 @@ val featureModule = module {
     single { NetworkClientService.providerService(MovieApi::class.java) }
     single { MovieLocalDataSource(get()) }
     single { MovieRemoteDataSource(get()) }
-    single { MovieRepositoryImpl(get(), get()) }
+    single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+    factory { MovieUseCase(get()) }
+    factory { DetailUseCase(get()) }
+    viewModel { MovieViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
 }
