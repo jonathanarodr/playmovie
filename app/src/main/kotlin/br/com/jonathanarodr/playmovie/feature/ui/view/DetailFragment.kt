8272b7b0dde4fb53e2fb8a7a76fa9b83a3c9e60c
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-07-14 16:14:06 -0300

    Remove legacy code

--- app/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/ui/view/DetailFragment.kt
+++
@@ -1,8 +0,0 @@
-package br.com.jonathanarodr.playmovie.feature.ui.view
-
-import androidx.fragment.app.Fragment
-import br.com.jonathanarodr.playmovie.R
-
-class DetailFragment : Fragment(R.layout.fragment_detail) {
-
-}
