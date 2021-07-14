81a27a34400647e9429f4ee56962407b34fed995
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-07-14 12:28:46 -0300

    Remove legacy java code

--- app/src/main/java/br/com/jonathanarodr/playmovie/ui/MovieAdapterOnClickHandler.kt
+++
@@ -1,7 +0,0 @@
-package br.com.jonathanarodr.playmovie.ui
-
-import br.com.jonathanarodr.playmovie.model.Movie
-
-interface MovieAdapterOnClickHandler {
-    fun onClick(movie: Movie)
-}
--- app/src/main/java/br/com/jonathanarodr/playmovie/util/GlideUtils.kt
+++
@@ -1,9 +0,0 @@
-package br.com.jonathanarodr.playmovie.util
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideUtils : AppGlideModule() {
-    //nothing
-}
