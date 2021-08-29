07bffea2011de1029bdceabe29bee835e573a4e7
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-08-29 11:31:22 -0300

    Add module configuration

---
+++ buildSrc/src/main/kotlin/config/Modules.kt
@@ -0,0 +1,8 @@
+package config
+
+object Modules {
+
+    const val COMMON = ":common"
+    const val NETWORK = ":network"
+    const val FEATURE = ":feature"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 include(
     ":app",
-    ":network",
     ":common",
+    ":network",
 )
