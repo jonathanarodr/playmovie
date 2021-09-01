dcc0ea846405b5f50e9443eb4c681a17ba01706f
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2021-08-31 23:50:30 -0300

    Add android config class

---
+++ buildSrc/src/main/kotlin/config/AndroidConfig.kt
@@ -0,0 +1,15 @@
+package config
+
+object AndroidConfig {
+
+    const val APPLICATION_ID = "br.com.jonathanarodr.playmovie"
+
+    const val SDK_COMPILER = 31
+    const val SDK_TARGET = 31
+    const val SDK_MINIMUM = 21
+
+    const val BUILD_TOOLS = "31.0.0"
+
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "2.0.0"
+}
