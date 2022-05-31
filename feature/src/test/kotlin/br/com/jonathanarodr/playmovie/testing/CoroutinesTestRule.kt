11dc4f48300f652f34e11caa1d3a14f9c9207dbf
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2022-05-30 22:37:28 -0300

    Replace runBlockingTest to runTest

--- feature/src/test/kotlin/br/com/jonathanarodr/playmovie/testing/CoroutinesTestRule.kt
+++ feature/src/test/kotlin/br/com/jonathanarodr/playmovie/testing/CoroutinesTestRule.kt
@@ -2,17 +2,22 @@ package br.com.jonathanarodr.playmovie.testing
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class CoroutinesTestRule(
-    val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : TestWatcher() {
+
+    val scope by lazy {
+        TestScope(dispatcher)
+    }
 
     override fun starting(description: Description?) {
         super.starting(description)
@@ -22,13 +27,5 @@ class CoroutinesTestRule(
     override fun finished(description: Description?) {
         super.finished(description)
         Dispatchers.resetMain()
-        dispatcher.cleanupTestCoroutines()
-    }
-}
-
-@ExperimentalCoroutinesApi
-fun CoroutinesTestRule.runBlockingTest(block: suspend CoroutinesTestRule.() -> Unit) {
-    return this.dispatcher.runBlockingTest {
-        block()
     }
 }
