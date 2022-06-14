c8f20778502940d78b32b4a56cac6499c582b471
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2022-06-14 10:54:42 -0300

    Removed legacy base use case

--- feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/domain/usecase/DetailUseCase.kt
+++ feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/domain/usecase/DetailUseCase.kt
@@ -1,28 +1,21 @@
 package br.com.jonathanarodr.playmovie.feature.domain.usecase
 
-import br.com.jonathanarodr.playmovie.common.usecase.UseCase
 import br.com.jonathanarodr.playmovie.feature.domain.model.Movie
 import br.com.jonathanarodr.playmovie.feature.repository.MovieRepository
 
 class DetailUseCase(
     private val movieRepository: MovieRepository,
-) : UseCase() {
+) {
 
     suspend fun getFavoriteMovie(movieId: Long): Result<Movie> {
-        return execute {
-            movieRepository.getFavoriteMovie(movieId)
-        }
+        return movieRepository.getFavoriteMovie(movieId)
     }
 
     suspend fun insertFavoriteMovie(movie: Movie): Result<Unit> {
-        return execute {
-            movieRepository.insertFavoriteMovie(movie)
-        }
+        return movieRepository.insertFavoriteMovie(movie)
     }
 
     suspend fun removeFavoriteMovie(movie: Movie): Result<Unit> {
-        return execute {
-            movieRepository.removeFavoriteMovie(movie)
-        }
+        return movieRepository.removeFavoriteMovie(movie)
     }
 }
--- feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/domain/usecase/MovieUseCase.kt
+++ feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/domain/usecase/MovieUseCase.kt
@@ -1,28 +1,21 @@
 package br.com.jonathanarodr.playmovie.feature.domain.usecase
 
-import br.com.jonathanarodr.playmovie.common.usecase.UseCase
 import br.com.jonathanarodr.playmovie.feature.domain.model.Movie
 import br.com.jonathanarodr.playmovie.feature.repository.MovieRepository
 
 class MovieUseCase(
     private val movieRepository: MovieRepository,
-) : UseCase() {
+) {
 
     suspend fun searchMovies(): Result<List<Movie>> {
-        return execute {
-            movieRepository.searchMovies()
-        }
+        return movieRepository.searchMovies()
     }
 
     suspend fun searchTvSeries(): Result<List<Movie>> {
-        return execute {
-            movieRepository.searchTvSeries()
-        }
+        return movieRepository.searchTvSeries()
     }
 
     suspend fun searchFavoriteMovies(): Result<List<Movie>> {
-        return execute {
-            movieRepository.searchFavoriteMovies()
-        }
+        return movieRepository.searchFavoriteMovies()
     }
 }
