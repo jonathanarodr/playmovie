940f51c81a09ed02289d5f4cdc305b1466381c0c
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2022-06-15 14:07:48 -0300

    Removed duplicated dependencies

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -6,28 +6,22 @@ plugins {
 dependencies {
     implementation(LibraryDependency.LIFECYCLE_LIVEDATA)
 
+    androidTestUtil(TestDependency.ORCHESTRATOR)
+
     api(TestDependency.JUNIT)
     api(TestDependency.JUNIT_EXT)
     api(TestDependency.TRUTH_CORE)
     api(TestDependency.TRUTH_EXT)
     api(TestDependency.MOCKK)
+    api(TestDependency.MOCKK_ANDROID)
     api(TestDependency.COROUTINES)
     api(TestDependency.ARCH)
     api(TestDependency.RUNNER)
     api(TestDependency.RULES)
     api(TestDependency.ROOM)
     api(TestDependency.ROBOLECTRIC_CORE)
-
-    androidTestUtil(TestDependency.ORCHESTRATOR)
-    androidTestImplementation(TestDependency.RUNNER)
-    androidTestImplementation(TestDependency.RULES)
-    androidTestImplementation(TestDependency.JUNIT_EXT)
-    androidTestImplementation(TestDependency.TRUTH_CORE)
-    androidTestImplementation(TestDependency.TRUTH_EXT)
-    androidTestImplementation(TestDependency.COROUTINES)
-    androidTestImplementation(TestDependency.MOCKK_ANDROID)
-    androidTestImplementation(TestDependency.ROBOLECTRIC_ANNOTATIONS)
-    androidTestImplementation(TestDependency.ESPRESSO) {
+    api(TestDependency.ROBOLECTRIC_ANNOTATIONS)
+    api(TestDependency.ESPRESSO) {
         exclude(group = "com.android.support", module = "support-annotations")
     }
 }
