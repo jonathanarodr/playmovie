b064569c7903808b857ed39ae476b0e0772a06ee
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2022-06-27 22:31:46 -0300

    Add compose dependency in common module

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,10 +4,21 @@ plugins {
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
+android {
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Version.Jetpack.COMPOSE
+    }
+}
+
 dependencies {
     implementation(LibraryDependency.KOIN)
     implementation(LibraryDependency.TIMBER)
     implementation(LibraryDependency.COIL)
+
+    implementation(LibraryDependency.COMPOSE_UI)
 
     testImplementation(project(Modules.TESTING))
 }
