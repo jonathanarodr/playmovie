0b2794ca9af5098eff0559f306b1f75d682f2f46
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2022-06-27 22:32:01 -0300

    Add design tokens in compose

---
+++ common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/uikit/token/FontSize.kt
@@ -0,0 +1,14 @@
+package br.com.jonathanarodr.playmovie.common.uikit.token
+
+import androidx.compose.ui.unit.sp
+
+object FontSize {
+    val xxs = 12.sp
+    val xs = 14.sp
+    val sm = 16.sp
+    val md = 18.sp
+    val lg = 20.sp
+    val xl = 24.sp
+    val xxl = 36.sp
+    val xxxl = 40.sp
+}
