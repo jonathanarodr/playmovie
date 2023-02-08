fa2f83fb3d0361e157869b34938d323278504533
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-08 00:27:03 -0300

    Adding temporary config to setup base compile dependencies

--- gradle-build/plugins/build.gradle.kts
+++ gradle-build/plugins/build.gradle.kts
@@ -3,3 +3,18 @@ plugins {
 }
 
 group = "br.com.jonathanarodr.playmovie.gradlebuild"
+
+/**
+ * FIXME use the catalog version
+ * https://github.com/jonathanarodr/playmovie/issues/41
+ */
+dependencies {
+    compileOnly("com.android.tools.build:gradle:7.4.1")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+}
+
+gradlePlugin {
+    plugins {
+        //todo
+    }
+}
