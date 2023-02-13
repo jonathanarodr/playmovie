a24bab8514856afb1462519fb7db90609deaeb7c
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-12 22:03:26 -0300

    Adding the build src code in gradle build module

---
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/ServerConfig.kt
@@ -0,0 +1,7 @@
+package br.com.jonathanarodr.playmovie.gradlebuild.config
+
+object ServerConfig {
+
+    const val URL = "https://api.themoviedb.org"
+    const val AUTHORIZATION = "[my-authorization-key]"
+}
---
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/dependencies/Version.kt
@@ -0,0 +1,67 @@
+package br.com.jonathanarodr.playmovie.gradlebuild.dependencies
+
+class Version {
+
+    object Gradle {
+        //also change in gradle-build
+        const val ANDROID_PLUGIN = "7.4.1"
+    }
+
+    object Kotlin {
+        //also change in gradle-build
+        const val CORE = "1.7.20"
+        const val COROUTINES = "1.6.0"
+    }
+
+    object Jetpack {
+        const val CORE = "1.7.0"
+        const val APPCOMPAT = "1.4.0"
+        const val ACTIVITY_COMPOSE = "1.5.1"
+        const val RECYCLER = "1.2.1"
+        const val CONSTRAINT = "2.1.2"
+        const val SWIPE_REFRESH = "1.1.0"
+        const val LIFECYCLE = "2.5.1"
+        const val ROOM = "2.4.2"
+        const val NAVIGATION = "2.5.3"
+    }
+
+    object Compose {
+        const val CORE = "2022.10.00"
+        const val COMPILER = "1.3.2"
+        const val ACCOMPANIST = "0.27.0"
+    }
+
+    object Square {
+        const val RETROFIT = "2.9.0"
+        const val OKHTTP = "4.9.2"
+        const val LEAKCANARY = "2.7"
+    }
+
+    object Tools {
+        const val MATERIAL = "1.4.0"
+        const val GSON = "2.8.9"
+        const val TIMBER = "4.7.1"
+        const val KOIN = "3.1.2"
+        const val COIL = "2.2.2"
+    }
+
+    object Lint {
+        const val DETEKT = "1.22.0-RC3"
+        const val KTLINT = "10.3.0"
+    }
+
+    object Test {
+        const val KOVER = "0.4.2"
+        const val JUNIT = "4.13.2"
+        const val JUNIT_EXT = "1.1.3"
+        const val TRUTH = "1.1.3"
+        const val TRUTH_EXT = "1.4.0"
+        const val MOCKK = "1.12.1"
+        const val ARCH = "1.6.0"
+        const val ORCHESTRATOR = "1.4.0"
+        const val RUNNER = "1.4.0"
+        const val RULES = "1.4.0"
+        const val ROBOLECTRIC = "4.5.1"
+        const val ESPRESSO = "3.4.0"
+    }
+}
