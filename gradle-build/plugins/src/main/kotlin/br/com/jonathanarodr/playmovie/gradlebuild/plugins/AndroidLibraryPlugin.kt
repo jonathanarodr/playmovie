4984f7e21c72c1daa8edd3e0c11aac8724661650
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-16 23:10:05 -0300

    Adding android library plugin

---
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/AndroidLibraryPlugin.kt
@@ -0,0 +1,41 @@
+package br.com.jonathanarodr.playmovie.gradlebuild.plugins
+
+import br.com.jonathanarodr.playmovie.gradlebuild.apply
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureAndroidLibraryConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureAndroidTestConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureComposeConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureDeviceManagerConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureKotlinConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureSourceConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureUnitTestConvention
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidLibraryPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply(
+                "com.android.library",
+                "kotlin-android",
+                "kotlin-parcelize",
+                "com.google.devtools.ksp",
+//                "androidx.navigation.safeargs.kotlin",
+            )
+            extensions.configure<LibraryExtension> {
+                configureAndroidLibraryConvention(this)
+                configureKotlinConvention(this)
+                configureUnitTestConvention(this)
+                configureAndroidTestConvention(this)
+                configureComposeConvention(this)
+                configureDeviceManagerConvention(this)
+            }
+            extensions.configure<LibraryAndroidComponentsExtension> {
+                configureSourceConvention(this)
+            }
+        }
+    }
+}
