eb4c5ffbae8f03214c92e9491e4f67fe2a24d938
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-16 23:09:00 -0300

    Adding kotlin library plugin

---
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/KotlinLibraryPlugin.kt
@@ -0,0 +1,21 @@
+package br.com.jonathanarodr.playmovie.gradlebuild.plugins
+
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureKotlinConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureUnitTestConvention
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class KotlinLibraryPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("kotlin")
+            extensions.configure<LibraryExtension> {
+                configureKotlinConvention(this)
+                configureUnitTestConvention(this)
+            }
+        }
+    }
+}
