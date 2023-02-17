c26e4c9dabaac62f2e301dafa9a46e9844025f96
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-17 08:44:24 -0300

    Registering conventional plugins

--- gradle-build/plugins/build.gradle.kts
+++ gradle-build/plugins/build.gradle.kts
@@ -10,11 +10,34 @@ group = "br.com.jonathanarodr.playmovie.gradlebuild"
  */
 dependencies {
     compileOnly("com.android.tools.build:gradle:7.4.1")
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+    compileOnly("org.jetbrains.kotlinx:kover:0.6.1")
+    compileOnly("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.7.21-1.0.8")
+    compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
+    compileOnly("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
 }
 
 gradlePlugin {
     plugins {
-        //todo
+        register("android-application") {
+            id = "playmovie.android.application"
+            implementationClass = "br.com.jonathanarodr.playmovie.gradlebuild.plugins.AndroidApplicationPlugin"
+        }
+        register("android-library") {
+            id = "playmovie.android.library"
+            implementationClass = "br.com.jonathanarodr.playmovie.gradlebuild.plugins.AndroidLibraryPlugin"
+        }
+        register("kotlin-library") {
+            id = "playmovie.kotlin.library"
+            implementationClass = "br.com.jonathanarodr.playmovie.gradlebuild.plugins.KotlinLibraryPlugin"
+        }
+        register("code-style") {
+            id = "playmovie.codestyle"
+            implementationClass = "br.com.jonathanarodr.playmovie.gradlebuild.plugins.CodeStylePlugin"
+        }
+        register("code-coverage") {
+            id = "playmovie.codecoverage"
+            implementationClass = "br.com.jonathanarodr.playmovie.gradlebuild.plugins.CodeCoveragePlugin"
+        }
     }
 }
