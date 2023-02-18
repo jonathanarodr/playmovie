75f7757b113965f18cc6062c1703f00f6bdbdb09
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-18 10:40:03 -0300

    Removing temporary compose dependency

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("playmovie.android.module")
-    id("playmovie.android.compose")
 }
 
 android {
@@ -11,20 +10,11 @@ dependencies {
     implementation(project(Modules.COMMON))
     implementation(project(Modules.NETWORK))
 
-    implementation(LibraryDependency.COROUTINES_CORE)
-    implementation(LibraryDependency.COROUTINES_ANDROID)
-    implementation(LibraryDependency.COROUTINES_PLAY_SERVICES)
-
     implementation(LibraryDependency.ANDROIDX_CORE)
     implementation(LibraryDependency.ANDROIDX_APPCOMPAT)
     implementation(LibraryDependency.ANDROIDX_RECYCLER)
     implementation(LibraryDependency.ANDROIDX_CONSTRAINT)
     implementation(LibraryDependency.ANDROIDX_SWIPE_REFRESH)
-
-    implementation(LibraryDependency.LIFECYCLE_VIEWMODEL)
-    implementation(LibraryDependency.LIFECYCLE_LIVEDATA)
-    implementation(LibraryDependency.LIFECYCLE_RUNTIME)
-    implementation(LibraryDependency.LIFECYCLE_COMMON)
 
     implementation(LibraryDependency.ROOM_CORE)
     implementation(LibraryDependency.ROOM_RUNTIME)
@@ -34,9 +24,6 @@ dependencies {
     implementation(LibraryDependency.NAVIGATION_UI)
 
     implementation(LibraryDependency.MATERIAL)
-    implementation(LibraryDependency.GSON)
-    implementation(LibraryDependency.TIMBER)
-    implementation(LibraryDependency.KOIN)
 
     testImplementation(project(Modules.TESTING))
     androidTestImplementation(project(Modules.TESTING))
--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/AndroidComposePlugin.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/AndroidComposePlugin.kt
@@ -1,7 +1,8 @@
 package br.com.jonathanarodr.playmovie.gradlebuild.plugins
 
+import br.com.jonathanarodr.playmovie.gradlebuild.androidTestImplementation
 import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureComposeConvention
-import br.com.jonathanarodr.playmovie.gradlebuild.debugApi
+import br.com.jonathanarodr.playmovie.gradlebuild.debugImplementation
 import br.com.jonathanarodr.playmovie.gradlebuild.dependencies.LibraryDependency
 import br.com.jonathanarodr.playmovie.gradlebuild.implementation
 import com.android.build.gradle.LibraryExtension
@@ -21,14 +22,15 @@ class AndroidComposePlugin : Plugin<Project> {
                 implementation(platform(LibraryDependency.COMPOSE_BOM))
                 implementation(LibraryDependency.COMPOSE_RUNTIME)
                 implementation(LibraryDependency.COMPOSE_UI)
-                implementation(LibraryDependency.COMPOSE_PREVIEW)
                 implementation(LibraryDependency.COMPOSE_FOUNDATION)
                 implementation(LibraryDependency.COMPOSE_MATERIAL3)
                 implementation(LibraryDependency.COMPOSE_LIVEDATA)
-                implementation(LibraryDependency.COIL_CORE)
                 implementation(LibraryDependency.COIL_COMPOSE)
 
-                debugApi(LibraryDependency.COMPOSE_TOOLING)
+                debugImplementation(LibraryDependency.COMPOSE_PREVIEW)
+                debugImplementation(LibraryDependency.COMPOSE_TOOLING)
+
+                androidTestImplementation(platform(LibraryDependency.COMPOSE_BOM))
             }
         }
     }
