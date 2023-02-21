520e6038ae8d3309c5e3ae0e85b9d107b5e7134f
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-21 14:32:18 -0300

    Removing ktlint dependency (#51)

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/dependencies/LintDependency.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/dependencies/LintDependency.kt
@@ -4,6 +4,4 @@ object LintDependency {
 
     const val DETEKT_PLUGIN = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Version.Lint.DETEKT}"
     const val DETEKT_FORMATTING = "io.gitlab.arturbosch.detekt:detekt-formatting:${Version.Lint.DETEKT}"
-
-    const val KTLINT = "org.jlleitschuh.gradle:ktlint-gradle:${Version.Lint.KTLINT}"
 }
