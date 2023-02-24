a89149ec8723a8d0956ea174cd5dca4d1c281a14
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-02-23 23:09:59 -0300

    Deleting the version control legacy configured by buildSrc

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/Modules.kt
+++
@@ -1,9 +0,0 @@
-package br.com.jonathanarodr.playmovie.gradlebuild.config
-
-object Modules {
-
-    const val TESTING = ":testing"
-    const val COMMON = ":common"
-    const val NETWORK = ":network"
-    const val FEATURE = ":feature"
-}
