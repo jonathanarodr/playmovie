a3ed539274e2f981879f42e41d7905eeb7effd25
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-04-11 21:23:18 -0300

    Improvement extension failure when could not retrieve the exception value

--- common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/utils/ResultUtils.kt
+++ common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/utils/ResultUtils.kt
@@ -1,8 +1,16 @@
 package br.com.jonathanarodr.playmovie.common.utils
 
 import br.com.jonathanarodr.playmovie.common.exception.ResultException
+import timber.log.Timber
 
-inline fun <T> Result<T>.onFailure(action: (exception: ResultException) -> Unit): Result<T> {
-    getOrNull()?.let { action(it as ResultException) }
+inline fun <T> Result<T>.onFailure(action: (ResultException) -> Unit): Result<T> {
+    val exception = getOrNull() as? ResultException
+
+    if (exception != null) {
+        action(exception)
+    } else {
+        Timber.e("Could not retrieve the result exception with: Result ${getOrNull()}")
+    }
+
     return this
 }
