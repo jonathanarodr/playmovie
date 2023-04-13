b2175f2c8890a09b425a2bfdc33aa3ee05e5c02d
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-04-12 23:08:09 -0300

    Fixing return value to subscribe

--- common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/utils/FlowUtils.kt
+++ common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/utils/FlowUtils.kt
@@ -5,14 +5,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 
 private const val SUBSCRIBED_TIMEOUT: Long = 5_000
 
-fun <T> Flow<T>.stateWith(scope: CoroutineScope, initialValue: T) {
-    this.stateIn(
+fun <T> Flow<T>.stateWith(scope: CoroutineScope, initialValue: T): StateFlow<T> {
+    return this.stateIn(
         scope = scope,
         started = SharingStarted.WhileSubscribed(SUBSCRIBED_TIMEOUT),
         initialValue = initialValue,
