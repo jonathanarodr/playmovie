69889b1240fdf01b67ea1431e7969d3f863d37fb
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-04-12 22:42:03 -0300

    Creating the utility to use in flow

---
+++ common/src/main/kotlin/br/com/jonathanarodr/playmovie/common/utils/FlowUtils.kt
@@ -0,0 +1,26 @@
+package br.com.jonathanarodr.playmovie.common.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
+
+private const val SUBSCRIBED_TIMEOUT: Long = 5_000
+
+fun <T> Flow<T>.stateWith(scope: CoroutineScope, initialValue: T) {
+    this.stateIn(
+        scope = scope,
+        started = SharingStarted.WhileSubscribed(SUBSCRIBED_TIMEOUT),
+        initialValue = initialValue,
+    )
+}
+
+fun <T> Result<T>.asFlow(dispatcher: CoroutineDispatcher = Dispatchers.Default): Flow<T> {
+    return flow {
+        emit(getOrThrow())
+    }.flowOn(dispatcher)
+}
