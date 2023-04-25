644c52224cc0f5254fc7a9528ff2f5d395e35da3
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-04-25 08:08:07 -0300

    Bumping version code

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/AndroidConfig.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/AndroidConfig.kt
@@ -11,7 +11,7 @@ internal object AndroidConfig {
 
     const val BUILD_TOOLS = "33.0.2"
 
-    const val VERSION_CODE = 8
+    const val VERSION_CODE = 3
     const val VERSION_NAME = "3.12.0"
 
     const val JAVA_SOURCE_DIR = "src/main/java"
