c3f49537fdf49a2f26a7d8f10ce463fd4613f051
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-04-25 21:25:51 -0300

    Fixing response model

--- feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/repository/remote/api/MovieApi.kt
+++ feature/src/main/kotlin/br/com/jonathanarodr/playmovie/feature/repository/remote/api/MovieApi.kt
@@ -1,20 +1,19 @@
 package br.com.jonathanarodr.playmovie.feature.repository.remote.api
 
-import br.com.jonathanarodr.playmovie.feature.domain.model.Movie
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MovieApi {
 
     @GET("/3/movie/popular")
-    suspend fun searchMovies(): MovieResponse
+    suspend fun searchMovies(): DiscoverResponse
 
     @GET("/3/tv/popular")
-    suspend fun searchTvSeries(): MovieResponse
+    suspend fun searchTvSeries(): DiscoverResponse
 
     @GET("/3/movie/{movie_id}")
-    suspend fun getMovieDetail(@Path("movie_id") id: Long): Movie
+    suspend fun getMovieDetail(@Path("movie_id") id: Long): MovieResponse
 
     @GET("/3/tv/{tv_id}")
-    suspend fun getTvSerieDetail(@Path("tv_id") id: Long): Movie
+    suspend fun getTvSerieDetail(@Path("tv_id") id: Long): MovieResponse
 }
