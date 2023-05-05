55fe54ccf5d4987fb100a6c457a5268039b6be15
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2023-05-05 08:13:38 -0300

    Code coverage improvements (#128)

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/CodeCoveragePlugin.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/plugins/CodeCoveragePlugin.kt
@@ -1,9 +1,8 @@
 package br.com.jonathanarodr.playmovie.gradlebuild.plugins
 
-import br.com.jonathanarodr.playmovie.gradlebuild.convention.excludeDefaultClasses
-import br.com.jonathanarodr.playmovie.gradlebuild.convention.excludeDefaultTasks
-import kotlinx.kover.api.KoverMergedConfig
-import kotlinx.kover.api.KoverProjectConfig
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureCoverageConvention
+import br.com.jonathanarodr.playmovie.gradlebuild.convention.configureFilterConvention
+import kotlinx.kover.gradle.plugin.dsl.KoverReportExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -15,25 +14,9 @@ class CodeCoveragePlugin : Plugin<Project> {
         with(target) {
             pluginManager.apply("org.jetbrains.kotlinx.kover")
 
-            extensions.configure<KoverProjectConfig> {
-                verify {
-                    onCheck.set(false)
-                }
-                instrumentation {
-                    excludeDefaultTasks()
-                }
-            }
-
-            extensions.configure<KoverMergedConfig> {
-                enable()
-                verify {
-                    onCheck.set(false)
-                }
-                filters {
-                    classes {
-                        excludeDefaultClasses()
-                    }
-                }
+            extensions.configure<KoverReportExtension> {
+                configureCoverageConvention()
+                configureFilterConvention()
             }
         }
     }
