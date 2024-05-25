9a4f7ae1bcdd882cb42792a8b582ec7e02e3712e
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-05-24 22:51:07 -0300

    Fixed kotlin compiler with new convention used by jvm and android libraries

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/PlatformConfig.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/config/PlatformConfig.kt
@@ -1,9 +1,13 @@
 package br.com.jonathanarodr.playmovie.gradlebuild.config
 
 import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 internal data class PlatformConfig(
-    val javaVersion: JavaVersion = JavaVersion.VERSION_17,
+    val javaJvmVersion: JavaVersion = JavaVersion.VERSION_17,
+    val kotlinJvmVersion: JvmTarget = JvmTarget.JVM_17,
+    val allWarningsAsErrors: Boolean = false,
+    val freeCompilerArgs: String = "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
 )
 
 fun JavaVersion.versionName(): String = this.toString()
