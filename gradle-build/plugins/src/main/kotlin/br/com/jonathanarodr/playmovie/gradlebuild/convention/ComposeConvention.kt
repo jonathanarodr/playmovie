382524d24f28e41a99b0465bb25719b1383949dc
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-06-24 22:42:40 -0300

    feat: Update Compose compiler plugin (#302)

--- gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/convention/ComposeConvention.kt
+++ gradle-build/plugins/src/main/kotlin/br/com/jonathanarodr/playmovie/gradlebuild/convention/ComposeConvention.kt
@@ -2,6 +2,8 @@ package br.com.jonathanarodr.playmovie.gradlebuild.convention
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 fun Project.configureComposeConvention(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
@@ -10,5 +12,10 @@ fun Project.configureComposeConvention(
         buildFeatures {
             compose = true
         }
+    }
+
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        enableStrongSkippingMode.set(true)
+        stabilityConfigurationFile.set(rootProject.layout.projectDirectory.file("compose_compiler_config.conf"))
     }
 }
